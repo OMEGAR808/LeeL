7147db263577c8da324a4c4e2944a976467ec58b
nayhtetlin007 <143700605+OMEGAR808@users.noreply.github.com>
2023-12-19 06:39:32 +0630

    Update LeeL.py

--- LeeL.py
+++ LeeL.py
@@ -32,9 +32,9 @@ twf=[]
 ugen=[]
 ugen=[]
 for xd in range(10000):
-    aa='Mozilla/5.0 (Linux; Android 13;'
+    aa='Mozilla/5.0 (Windows NT 10.0; Win64; x64;'
     b=random.choice(['7.0','8.1.0','9','10','11','12'])
-    c=random.choice(['Redmi Note 10 Pro'])
+    c=random.choice(['Windows NT 10.0; Win64; x64'])
     d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
     e=random.randrange(1, 999)
     f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
@@ -43,7 +43,7 @@ for xd in range(10000):
     i='0'
     j=random.randrange(4200,4900)
     k=random.randrange(40,150)
-    l='Chrome/107.0.0.0 Mobile Safari/537.36'
+    l='Chrome/109.0.0.0 Safari/537.36 Vivaldi/5.6.2867.50'
     uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
     ugen.append(uaku2)
 def rndUA():
